5ca8f6c1c67c11f73772cd9424b0edf428d13f6f
EastonA01 <eastonredwing@gmail.com>
2024-11-15 16:49:41 -0500

    V2.2.1 : Hotfix, fixed headshot formatting/sizing weirdness

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const AboutSection = styled(motion.section)`
   display: flex;
+  flex-wrap: wrap;
   justify-content:center;
   align-items: center;
   gap: 30px;
